0a73eb7ebb1b11f6d873363e56b0a5f06733fd17
TRIUMFO\web1 <d.mkt@triumfo.de>
2025-11-13 19:42:59 +0530

    fevicon;

--- app/layout.js
+++ app/layout.js
@@ -69,7 +69,8 @@ export default function RootLayout({ children }) {
         href="/fonts/poppins.woff2"
         crossOrigin="anonymous"
       />
-
+        <link rel="icon" href="/favicon.png" />
+        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
