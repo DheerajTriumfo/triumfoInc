66920fa9456f8cc491effc480152b62fe296b09f
TRIUMFO\Webdev <php@triumfo.de>
2025-11-19 11:43:37 +0530

    data change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { buildMetadata } from '../lib/seo';
 
 export async function generateMetadata() {
   return await buildMetadata({
-    title: "Trade Show Booth Design & Exhibit Builders Usa.",
+    title: "Trade Show Booth Design & Exhibit Builders USA.",
     description:
       "Designing and building trade show booths for national and international exhibitors across the USA. Choose from over 500 booth designs.",
     pathname: "/",
